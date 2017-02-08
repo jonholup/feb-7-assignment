3335d37cbbaa1c9b7403b6a0b75ec9f34238f818
Jonny Holupchinski <jonholup@gmail.com>
2017-02-08 10:26:36 -0600

    set homeWork for loop index to start at numberand eliminated if loop

--- client.js
+++ client.js
@@ -3,22 +3,21 @@ function homeWork(array, number) { //defined function taking in specified parame
     console.warn(number + ' is greater than array length');
     return;
   }
-
-  array = array.slice(number); //slice uses our number parameter as a new start index. eliminates indexes prior, but does not modify so had to redefine array.
-  return array.join(''); //join takes our array elements and turns them nicely into a string! wow!
+  // also could've been written like -->
+  // array = array.slice(number); //slice uses our number parameter as a new start index. eliminates indexes prior, but does not modify so had to redefine array.
+  // return array.join(''); //join takes our array elements and turns them nicely into a string! wow!
 }
 
-// also could've been written like -->
-// function homeWork(array, number) {
-//   var newString = '';
-//   for (var i = 0; i < array.length; i++) {
-//     if (i >= number) {
-//       newString = newString + array[i];
-//     }
-//   }
-//   return newString;
-// }
-//
+
+function homeWork(array, number) {
+  var newString = '';
+  for (var i = number; i < array.length; i++) {
+      newString = newString + array[i];
+    
+  }
+  return newString;
+}
+
 
 console.log(homeWork(["ASDF", "BLAH", "Oh! ", "Hi ", "There!"], 3)); // Hi There!
 console.log(homeWork(["ASDF", "BLAH", "Oh! ", "Hi ", "There!"], 2)); // Oh! Hi There!
